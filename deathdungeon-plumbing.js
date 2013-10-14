b462e76355dac746c6e2d487c4810e69bceaf98f
Braun Goodson <braungoodson@gmail.com>
2013-10-13 20:48:13 -0400

    ok

--- deathdungeon-plumbing.js
+++ deathdungeon-plumbing.js
@@ -1,3 +1,12 @@
+var clc = require('cli-color')
+var error = clc.red.bold;
+var warn = clc.yellow;
+var notice = clc.cyan;
+var deathdungeon = clc.red;
+var say = clc.green;
+console.log(deathdungeon('deathdungeon:')+' dungeon is open');
+var fs = require('fs');
+var players = [];
 module.exports = {
 	port: 24000,
 	http: {
